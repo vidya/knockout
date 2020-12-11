45ae52124d836e1b841be9727caf060bdf025e5c
vidya <vsagarj@gmail.com>
2020-12-11 10:25:11 -0800

    Create the first mover radio buttons.

--- src/components/NimZap/index.js
+++ src/components/NimZap/index.js
@@ -4,10 +4,42 @@
 
 import React, { useState, useEffect } from 'react'
 
+import {ToggleButton} from 'react-bootstrap'
+import {ButtonGroup} from 'react-bootstrap'
+
+
 export default function NimZap(props) {
+    const [checked, setChecked] = useState(false);
+    const [radioValue, setRadioValue] = useState('1');
+
+    const radios = [
+        { name: 'PLAYER', value: '1' },
+        { name: 'COMPUTER', value: '2' },
+    ];
+
     console.log("NimZap: start")
 
     return (
-        <h1> Let the Games Begin!</h1>
-    )
+        <>
+            <fieldset>
+                <legend>First mover</legend>
+                <ButtonGroup toggle>
+                    {radios.map((radio, idx) => (
+                        <ToggleButton
+                            key={idx}
+                            type="radio"
+                            variant="secondary"
+                            name="radio"
+                            value={radio.value}
+                            checked={radioValue === radio.value}
+                            onChange={(e) => setRadioValue(e.currentTarget.value)}
+                        >
+                            {radio.name}
+                        </ToggleButton>
+
+                    ))}
+                </ButtonGroup>
+            </fieldset>
+        </>
+    );
 }
