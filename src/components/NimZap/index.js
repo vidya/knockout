4650e1e40215cc2afe4fe15d36793ea366868720
vidya <vsagarj@gmail.com>
2020-12-11 09:54:51 -0800

    First version.

---
+++ src/components/NimZap/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by J.Vidya Sagar on 12/10/20.
+ */
+
+import React, { useState, useEffect } from 'react'
+
+export default function NimZap(props) {
+    console.log("NimZap: start")
+
+    return (
+        <h1> Let the Games Begin!</h1>
+    )
+}
