a50c4d58f315a5800c16955e7bbeb952eed0db8e
vidya <vsagarj@gmail.com>
2020-12-13 00:00:11 -0800

    First version of recording moves on a ticker tape.
    Signed-off-by: vidya <vsagarj@gmail.com>

--- src/components/GameBoard/index.js
+++ src/components/GameBoard/index.js
@@ -7,6 +7,8 @@ import React, { useState } from 'react'
 import CoinHeap from "../CoinHeap";
 
 import './style.scss'
+import MovesRecord from "../MovesRecord";
+import TickerTape from "../TickerTape";
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -18,6 +20,19 @@ export default function GameBoard(props) {
     console.log("GameBoard: start")
     const [coinCounts, setCoinCounts] = useState([...Array(5).keys()].map(n => getRandomInt(1, 10)));
 
+    let moveInfo = {
+        'Move #': 0,
+        'Made by': 'player-08',
+        'Before': 'A2-B7-C6-D2-E4',
+        'Move': 'B2',
+        'After': 'A2-B5-C6-D2-E4',
+    }
+
+    const names = ['A', 'B', 'C', 'D', 'E']
+
+    const [movesLog, setMovesLog] = useState([moveInfo]);
+    const [moveNum, setMoveNum] = useState(0);
+
     const updateCoinCounts = (heapNum, count) => {
         console.log(`updateCoinCounts: (heapNum, count) = (${heapNum}, ${count})`)
 
@@ -25,9 +40,22 @@ export default function GameBoard(props) {
         newCoinCounts[heapNum] -= count
         setCoinCounts([...Array(5).keys()].map(n => newCoinCounts[n]))
         console.log(`updateCoinCounts: (newCoinCounts, coinCounts) = (${newCoinCounts}, ${coinCounts})`)
+
+        const newMoveNum  = moveNum  + 1
+        setMoveNum(newMoveNum)
+        moveInfo = {
+            'Move #': newMoveNum,
+            'Made by': 'Player',
+            'Before': 'A2-B7-C6-D2-E4',
+            'Move': `${names[heapNum]}${count}`,
+            'After': 'A2-B5-C6-D2-E4',
+        }
+        let newMovesLog = Array.from(movesLog)
+        newMovesLog.push(moveInfo)
+        setMovesLog(newMovesLog)
     }
 
-    const names = ['A', 'B', 'C', 'D', 'E']
+    // const names = ['A', 'B', 'C', 'D', 'E']
     let heapMap = new Map()
 
     for (const [index, name] of names.entries()) {
@@ -38,6 +66,10 @@ export default function GameBoard(props) {
 
     return (
         <div className={'gameBoardContainer'}>
+            <MovesRecord/>
+
+            <TickerTape  movesLog={movesLog}/>
+
             <div className={'gameBoard'}>
                 {
                     Array.from(heapMap.keys()).map(key =>
